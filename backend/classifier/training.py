d7c592311328c24a3c081a8642914988b4d82c80
Dork Legend <autodestruction@hotmail.com>
2019-09-30 00:58:52 +0100

    made small changes

--- backend/classifier/training.py
+++ backend/classifier/training.py
@@ -1,9 +1,6 @@
 
-# coding: utf-8
 
-# In[1]:
-
-
+# importing library
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
@@ -12,45 +9,24 @@ import numpy as np
 import json
 import dump
 
-
-# In[2]:
-
-
+# loading the npy files for training of model
 X_train = np.load('../dataset/X_train.npy')
 y_train = np.load('../dataset/y_train.npy')
 print('X_train:{0}, y_train:{1}'.format(X_train.shape, y_train.shape))
 
-
-# In[3]:
-
-
+# Using CLF for random forest, calculation for CV is calculated
 clf = RandomForestClassifier()
 print('Cross Validation Score: {0}'.format(np.mean(cross_val_score(clf, X_train, y_train, cv=10))))
 
-
-# In[4]:
-
-
 clf.fit(X_train, y_train)
 
-
-# In[5]:
-
-
+# loading test to calculate the accuracy of the model is calculated
 X_test = np.load('../dataset/X_test.npy')
 y_test = np.load('../dataset/y_test.npy')
-
-
-# In[6]:
-
 
 pred = clf.predict(X_test)
 print('Accuracy: {}'.format(accuracy_score(y_test, pred)))
 
-
-# In[7]:
-
-
-#print(forest_to_json(clf))
+# print(forest_to_json(clf))
 json.dump(dump.forest_to_json(clf), open('../../static/classifier.json', 'w'))
 
